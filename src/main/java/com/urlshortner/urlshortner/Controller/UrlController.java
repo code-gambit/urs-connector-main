13b57bf786d70729247d00b5ea47933503d8a57f
Danish Jamal <31315800+danishjamal104@users.noreply.github.com>
2022-03-11 00:01:18 +0530

    Major code updates and optimisation

--- src/main/java/com/urlshortner/urlshortner/Controller/UrlController.java
+++ src/main/java/com/urlshortner/urlshortner/Controller/UrlController.java
@@ -1,44 +1,74 @@
 package com.urlshortner.urlshortner.Controller;
+
 import com.urlshortner.urlshortner.Service.UrlService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "URS/api/test/v1/url")
+@RequestMapping(path = "api/v1/url")
 public class UrlController {
 
     private final UrlService urlService;
-    @Autowired
+
     public UrlController(UrlService urlService){
         this.urlService=urlService;
     }
 
+    /**
+     * HTTP POST request for getting short url id for the long url id passed in request body
+     *
+     * sample endpoint: http://localhost:8080/api/v1/url/
+     *
+     * sample request body:
+     *  {
+     *      "url": "http://google.com"
+     *  }
+     *
+     * sample response body:
+     *  {
+     *      "id": "1L9zO9O"
+     *  }
+     *
+     * @param body
+     * @return short url id corresponding to the long url
+     */
     @PostMapping(path = "")
-    public Map<String,String> createShortUrlFromLongUrl(@RequestBody Map<String,String> url){
-        return urlService.createShortUrlFromLongUrl(url.get("url"));//url is the long url for which will get short url
+    public Map<String,String> createShortUrlFromLongUrl(@RequestBody Map<String,String> body){
+        String shortId = urlService.createShortUrlFromLongUrl(body.get("url"));
+        Map<String, String> response = new HashMap<>();
+        response.put("id", shortId);
+        return response; //url is the long url for which will get short url
     }
 
+    /**
+     * HTTP GET request for fetching long url corresponding to the short ur id passed as path variable
+     *
+     * sample endpoint: http://localhost:8080/api/v1/url/1L9zO9O
+     *
+     * sample response body:
+     *  {
+     *      "url": "http://google.com"
+     *  }
+     *
+     * @param shortUrl shortUrl passed as path variable in endpoint
+     * @return short url id corresponding to the long url
+     */
     @GetMapping(path = "/{shortUrl}")
-    public RedirectView getLongUrlFromShortUrl(@PathVariable("shortUrl")String shortUrl){
-        Map<String,String> result=urlService.getLongUrlFromShortUrl(shortUrl);
-        RedirectView redirectView=new RedirectView();
-        String url=result.get("url");
-        if(!url.equals("URL not available")) {
-            redirectView.setUrl(url);
-            return redirectView;//It will redirect the user to mapped long url of the hash he passes.
-        }
-        else {
-            return null;
-        }
+    public Map<String, String> getLongUrlFromShortUrl(@PathVariable("shortUrl") String shortUrl){
+        Map<String, String> response = new HashMap<>();
+        String url = urlService.getLongUrlFromShortUrl(shortUrl);
+        response.put("url", url);
+        return response;
     }
 
-    @PutMapping(path = "/{urlid}")
+    /*@PutMapping(path = "/{urlid}")
     public Map<String,String> changeID(@RequestBody Map<String,String> id,@PathVariable("urlid")String urlid){
         //urlid is the id which is recently stored as a hash for the long url and id is the new id we want to replace
         // the recent one with
         return urlService.changeId(urlid,id.get("id"));
-    }
+    }*/
 
 }
